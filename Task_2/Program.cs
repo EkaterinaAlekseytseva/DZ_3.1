b73f150205b8daba90e84a8c10856c915172b43c
Ekaterina <alexeitseva.katya@yandex.ru>
2022-10-20 19:09:11 +0700

    Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает (целых) два числа и выдаёт (в терминале), какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число");
+int number_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int number_2 = int.Parse(Console.ReadLine()!);
+
+if(number_1 > number_2)
+{
+    Console.WriteLine(number_1);
+}
+else
+{
+    Console.WriteLine(number_2);
+}
