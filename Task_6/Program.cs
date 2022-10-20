fb67e3a981a1efdd1ed2f3801aef3e5e56cd21a4
Ekaterina <alexeitseva.katya@yandex.ru>
2022-10-20 20:58:08 +0700

    Task 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает (целое) число и выдаёт (В терминале), является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.WriteLine("Программа нахождения четного числа");
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+//int n = number%10;
+//Console.WriteLine("Остаток" + number%10);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
